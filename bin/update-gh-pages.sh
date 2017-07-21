822ec9f10df374487fd88392cd86170dae2c11f0
Madhu Mohan Nelemane <mmnelemane@suse.com>
2017-07-21 15:32:28 +0200

    Added gh-pages css update script.

---
+++ bin/update-gh-pages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+safely () {
+    if ! "$@"; then
+        echo >&2 "Aborting due to failure of: $*"
+        exit 1
+    fi
+}
+
+safely git branch -f gh-pages HEAD
+safely git checkout gh-pages
+safely sass --update css/reveal-override.scss
+safely git add -f css/*.css css/*.map
+safely git commit -m'Latest .css and .css.map files for publishing via gh-pages'
+safely git push -f github gh-pages
+safely git checkout -
